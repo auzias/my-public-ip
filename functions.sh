bbbed48d14f7febb9cde95c8a844dfc754c3d2a9
auzias <github@auzias.net>
2015-05-07 14:45:41 +0200

    Bunch of websites added

--- functions.sh
+++ functions.sh
@@ -1,6 +1,73 @@
 #!/bin/bash
 ALL=""
 
+##Web methods
+ALL="$ALL whatismyip"
+whatismyip() {
+  IP=$(curl --silent whatismyip.akamai.com)
+}
+
+ALL="$ALL whatismijnip"
+whatismijnip() {
+  IP=$(curl --silent http://whatismijnip.nl | \
+	cut -d " " -f 5)
+}
+
+ALL="$ALL icanhazip"
+icanhazip() {
+  IP=$(curl --silent https://icanhazip.com/)
+}
+
+ALL="$ALL curlmyip"
+curlmyip() {
+  IP=$(curl --silent http://curlmyip.com)
+}
+
+ALL="$ALL ident"
+ident() {
+  IP=$(curl --silent http://ident.me/)
+}
+
+ALL="$ALL ident"
+ifconfig() {
+  IP=$(curl --silent ifconfig.me)
+}
+
+ALL="$ALL tnx"
+tnx() {
+  IP=$(curl --silent tnx.nl/ip)
+}
+
+ALL="$ALL ipecho"
+ipecho() {
+  IP=$(curl --silent http://ipecho.net/plain)
+}
+
+ALL="$ALL ipapp"
+ipapp() {
+  IP=$(curl --silent ip.appspot.com)
+}
+
+ALL="$ALL wgetip"
+wgetip() {
+  IP=$(curl --silent wgetip.com)
+}
+
+ALL="$ALL iptyk"
+iptyk() {
+  IP=$(curl --silent ip.tyk.nu)
+}
+
+ALL="$ALL corz"
+corz() {
+  IP=$(curl --silent corz.org/ip)
+}
+
+ALL="$ALL whatismyipaddress"
+whatismyipaddress() {
+  IP=$(curl --silent bot.whatismyipaddress.com)
+}
+
 ALL="$ALL ipinfo"
 ipinfo() {
   IP=$(curl --silent http://ipinfo.io/ip)
@@ -27,6 +94,13 @@ monip() {
 	cut -d '<' -f 1 )
 }
 
+##DNS method
+ALL="$ALL dns"
+dns() {
+  IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+}
+
+##STUN method
 ALL="$ALL stun"
 stun() {
     IP=$(stun-client -v stun.voiparound.com 2>&1 1>/dev/null | \
@@ -34,3 +108,4 @@ stun() {
 	sed -e 's/.*MappedAddress = //' -e 's/:.*//' | \
 	uniq)
 }
+
